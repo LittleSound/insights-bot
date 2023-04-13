67a0ca6681fa5be93a8c23b594611dd4e8dc5a9e
Neko Ayaka <neko@ayaka.moe>
2023-04-13 14:41:20 +0800

    feat: added logger (#1)

---
+++ packages/share/logger.ts
@@ -0,0 +1,34 @@
+import { createLogger, format, transports } from 'winston'
+import { blue, cyan, red, yellow } from 'colors/safe'
+
+function mapLevelColor(level: string): string {
+  switch (level) {
+    case '[info]':
+      return blue(level)
+    case '[warn]':
+      return yellow(level)
+    case '[error]':
+      return red(level)
+    default:
+      return cyan(level)
+  }
+}
+
+const customFormat = format.printf(({ level, message, module, timestamp }) => {
+  return `${timestamp} ${mapLevelColor(`[${level}]`)}${module ? ` [${module}]` : ''}: ${message}`
+})
+
+const _log = createLogger({
+  level: 'debug',
+  defaultMeta: { service: 'insights-bot' },
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.timestamp(),
+        customFormat,
+      ),
+    }),
+  ],
+})
+
+export const log = _log
